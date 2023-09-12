ee7d11c2c787d103c1349a49bdc50d92e28376fd
Edvaldo <35712346+wsy152@users.noreply.github.com>
2023-09-12 13:03:17 -0300

    configuração sercice creete e update clientes

---
+++ src/modules/customers/services/show_customer_service.ts
@@ -0,0 +1,28 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/app_error";
+import CustumerEntity from "../typeorm/entities/customer_entity";
+import { CustumerRepository } from "../typeorm/repositories/cutomers_repository";
+
+
+
+interface IRequest {
+  id: string;
+
+}
+
+
+export class ShowCutomerSercice {
+  public async execute({ id }: IRequest): Promise<CustumerEntity> {
+
+    const customerRepository = getCustomRepository(CustumerRepository);
+
+    const custumer = await customerRepository.findbyId(id);
+
+    if (!custumer) {
+      throw new AppError('Product is not found');
+    }
+
+    return custumer;
+
+  }
+}
---
+++ src/modules/customers/services/update_cutomer_service.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/app_error";
+import CustumerEntity from "../typeorm/entities/customer_entity";
+import { CustumerRepository } from "../typeorm/repositories/cutomers_repository";
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+
+}
+
+export default class UpdateCutomerSercice {
+  public async execute({ id, name, email }: IRequest): Promise<CustumerEntity> {
+
+    const customerRepository = getCustomRepository(CustumerRepository);
+
+    const customer = await customerRepository.findOne(id);
+
+    if (!customer) {
+      throw new AppError('User is not found');
+    }
+    const userUpdateEmail = await customerRepository.findbyEmail(email);
+
+    if (userUpdateEmail && userUpdateEmail.id === parseInt(id)) {
+      throw new AppError('There is already one user with this email');
+    }
+
+    customer.name = name;
+    customer.email = email;
+
+    await customerRepository.save(customer);
+
+    return customer;
+
+  }
+}
